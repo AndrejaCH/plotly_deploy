12451f628de6f100a0ce80f61b167d02e7799855
Andreja Ho <sfandreja@gmail.com>
2020-09-28 21:14:31 -0700

    add files

---
+++ data.js
@@ -0,0 +1,393 @@
+var data = [{
+  greekName: "Amphitrite",
+  greekSearchResults: 387000,
+  pair: "Amphitrite-Salacia",
+  romanName: "Salacia",
+  romanSearchResults: 47500
+},
+{
+  greekName: "Ananke",
+  greekSearchResults: 73600,
+  pair: "Ananke-Necessitas",
+  romanName: "Necessitas",
+  romanSearchResults: 386000
+},
+{
+  greekName: "Aphrodite",
+  greekSearchResults: 9540000,
+  pair: "Aphrodite-Venus",
+  romanName: "Venus",
+  romanSearchResults: 15700000
+},
+{
+  greekName: "Ares",
+  greekSearchResults: 469000,
+  pair: "Ares-Mars",
+  romanName: "Mars",
+  romanSearchResults: 14100000
+},
+{
+  greekName: "Artemis",
+  greekSearchResults: 10200000,
+  pair: "Artemis-Diana",
+  romanName: "Diana",
+  romanSearchResults: 15800000
+},
+{
+  greekName: "Asclepius",
+  greekSearchResults: 315000,
+  pair: "Asclepius-Aesculapius",
+  romanName: "Aesculapius",
+  romanSearchResults: 101000
+},
+{
+  greekName: "Asclepius",
+  greekSearchResults: 315000,
+  pair: "Asclepius-Veiovis",
+  romanName: "Veiovis",
+  romanSearchResults: 24200
+},
+{
+  greekName: "Athena",
+  greekSearchResults: 10200000,
+  pair: "Athena-Minerva",
+  romanName: "Minerva",
+  romanSearchResults: 437000
+},
+{
+  greekName: "Atropos",
+  greekSearchResults: 341000,
+  pair: "Atropos-Morta",
+  romanName: "Morta",
+  romanSearchResults: 468000
+},
+{
+  greekName: "Boreas",
+  greekSearchResults: 301000,
+  pair: "Boreas-Aquilo",
+  romanName: "Aquilo",
+  romanSearchResults: 451000
+},
+{
+  greekName: "Charites (Graces)",
+  greekSearchResults: 160000,
+  pair: "Charites (Graces)-Gratiae",
+  romanName: "Gratiae",
+  romanSearchResults: 273000
+},
+{
+  greekName: "Chloris",
+  greekSearchResults: 121000,
+  pair: "Chloris-Flora",
+  romanName: "Flora",
+  romanSearchResults: 12300000
+},
+{
+  greekName: "Clotho",
+  greekSearchResults: 272000,
+  pair: "Clotho-Nona",
+  romanName: "Nona",
+  romanSearchResults: 366000
+},
+{
+  greekName: "Cronus",
+  greekSearchResults: 449000,
+  pair: "Cronus-Saturn",
+  romanName: "Saturn",
+  romanSearchResults: 732000
+},
+{
+  greekName: "Demeter",
+  greekSearchResults: 451000,
+  pair: "Demeter-Ceres",
+  romanName: "Ceres",
+  romanSearchResults: 476000
+},
+{
+  greekName: "Dionysus",
+  greekSearchResults: 568000,
+  pair: "Dionysus-Bacchus",
+  romanName: "Bacchus",
+  romanSearchResults: 513000
+},
+{
+  greekName: "Enyo",
+  greekSearchResults: 89300,
+  pair: "Enyo-Bellona",
+  romanName: "Bellona",
+  romanSearchResults: 242000
+},
+{
+  greekName: "Eos",
+  greekSearchResults: 554000,
+  pair: "Eos-Aurora",
+  romanName: "Aurora",
+  romanSearchResults: 11800000
+},
+{
+  greekName: "Erinyes",
+  greekSearchResults: 175000,
+  pair: "Erinyes-Furies",
+  romanName: "Furies",
+  romanSearchResults: 401000
+},
+{
+  greekName: "Eris",
+  greekSearchResults: 436000,
+  pair: "Eris-Discordia",
+  romanName: "Discordia",
+  romanSearchResults: 317000
+},
+{
+  greekName: "Eros",
+  greekSearchResults: 542000,
+  pair: "Eros-Cupid (Amor)",
+  romanName: "Cupid (Amor)",
+  romanSearchResults: 587000
+},
+{
+  greekName: "Eurus",
+  greekSearchResults: 54600,
+  pair: "Eurus-Vulturnus",
+  romanName: "Vulturnus",
+  romanSearchResults: 9430
+},
+{
+  greekName: "Gaia",
+  greekSearchResults: 1230000,
+  pair: "Gaia-Tellus",
+  romanName: "Tellus",
+  romanSearchResults: 209000
+},
+{
+  greekName: "Galinthias",
+  greekSearchResults: 2460,
+  pair: "Galinthias-Galinthis",
+  romanName: "Galinthis",
+  romanSearchResults: 11300000
+},
+{
+  greekName: "Irene",
+  greekSearchResults: 628000,
+  pair: "Irene-Pax",
+  romanName: "Pax",
+  romanSearchResults: 608000
+},
+{
+  greekName: "Hades",
+  greekSearchResults: 9540000,
+  pair: "Hades-Dis Pater, Pluto, Orcus",
+  romanName: "Pluto",
+  romanSearchResults: 492000
+},
+{
+  greekName: "Hebe",
+  greekSearchResults: 381000,
+  pair: "Hebe-Juventas",
+  romanName: "Juventas",
+  romanSearchResults: 131000
+},
+{
+  greekName: "Hecate",
+  greekSearchResults: 496000,
+  pair: "Hecate-Trivia",
+  romanName: "Trivia",
+  romanSearchResults: 4460000
+},
+{
+  greekName: "Helios",
+  greekSearchResults: 501000,
+  pair: "Helios-Sol",
+  romanName: "Sol",
+  romanSearchResults: 11700000
+},
+{
+  greekName: "Hera",
+  greekSearchResults: 512000,
+  pair: "Hera-Juno",
+  romanName: "Juno",
+  romanSearchResults: 560000
+},
+{
+  greekName: "Heracles",
+  greekSearchResults: 506000,
+  pair: "Heracles-Hercules",
+  romanName: "Hercules",
+  romanSearchResults: 9140000
+},
+{
+  greekName: "Hermes",
+  greekSearchResults: 1890000,
+  pair: "Hermes-Mercury",
+  romanName: "Mercury",
+  romanSearchResults: 1870000
+},
+{
+  greekName: "Hephaestus",
+  greekSearchResults: 512000,
+  pair: "Hephaestus-Vulcan",
+  romanName: "Vulcan",
+  romanSearchResults: 475000
+},
+{
+  greekName: "Hesperos",
+  greekSearchResults: 55500,
+  pair: "Hesperos-Vesper",
+  romanName: "Vesper",
+  romanSearchResults: 272000
+},
+{
+  greekName: "Hestia",
+  greekSearchResults: 469000,
+  pair: "Hestia-Vesta",
+  romanName: "Vesta",
+  romanSearchResults: 408000
+},
+{
+  greekName: "Hygieia",
+  greekSearchResults: 321000,
+  pair: "Hygieia-Salus",
+  romanName: "Salus",
+  romanSearchResults: 331000
+},
+{
+  greekName: "Hypnos",
+  greekSearchResults: 386000,
+  pair: "Hypnos-Somnus",
+  romanName: "Somnus",
+  romanSearchResults: 162000
+},
+{
+  greekName: "Lachesis",
+  greekSearchResults: 272000,
+  pair: "Lachesis-Decima",
+  romanName: "Decima",
+  romanSearchResults: 247000
+},
+{
+  greekName: "Leto",
+  greekSearchResults: 518000,
+  pair: "Leto-Latona",
+  romanName: "Latona",
+  romanSearchResults: 106000
+},
+{
+  greekName: "Moirae (Fates)",
+  greekSearchResults: 94200,
+  pair: "Moirae (Fates)-Parcae",
+  romanName: "Parcae",
+  romanSearchResults: 430000
+},
+{
+  greekName: "Nike",
+  greekSearchResults: 1270000,
+  pair: "Nike-Victoria",
+  romanName: "Victoria",
+  romanSearchResults: 19400000
+},
+{
+  greekName: "Notus",
+  greekSearchResults: 254000,
+  pair: "Notus-Auster",
+  romanName: "Auster",
+  romanSearchResults: 195000
+},
+{
+  greekName: "Odysseus",
+  greekSearchResults: 441000,
+  pair: "Odysseus-Ulysses",
+  romanName: "Ulysses",
+  romanSearchResults: 500000
+},
+{
+  greekName: "Pan",
+  greekSearchResults: 14600000,
+  pair: "Pan-Faunus",
+  romanName: "Faunus",
+  romanSearchResults: 178000
+},
+{
+  greekName: "Persephone",
+  greekSearchResults: 1240000,
+  pair: "Persephone-Proserpina",
+  romanName: "Proserpina",
+  romanSearchResults: 368000
+},
+{
+  greekName: "Pheme",
+  greekSearchResults: 150000,
+  pair: "Pheme-Fama",
+  romanName: "Fama",
+  romanSearchResults: 419000
+},
+{
+  greekName: "Phosporus",
+  greekSearchResults: 257000,
+  pair: "Phosporus-Lucifer",
+  romanName: "Lucifer",
+  romanSearchResults: 729000
+},
+{
+  greekName: "Poseidon",
+  greekSearchResults: 4590000,
+  pair: "Poseidon-Neptune",
+  romanName: "Neptune",
+  romanSearchResults: 1280000
+},
+{
+  greekName: "Satyr",
+  greekSearchResults: 566000,
+  pair: "Satyr-Faun",
+  romanName: "Faun",
+  romanSearchResults: 560000
+},
+{
+  greekName: "Selene",
+  greekSearchResults: 525000,
+  pair: "Selene-Luna",
+  romanName: "Luna",
+  romanSearchResults: 14700000
+},
+{
+  greekName: "Semele",
+  greekSearchResults: 159000,
+  pair: "Semele-Stimula",
+  romanName: "Stimula",
+  romanSearchResults: 140000
+},
+{
+  greekName: "Silenus",
+  greekSearchResults: 172000,
+  pair: "Silenus-Silvanus",
+  romanName: "Silvanus",
+  romanSearchResults: 99200
+},
+{
+  greekName: "Themis",
+  greekSearchResults: 351000,
+  pair: "Themis-Justitia",
+  romanName: "Justitia",
+  romanSearchResults: 136000
+},
+{
+  greekName: "Tyche",
+  greekSearchResults: 332000,
+  pair: "Tyche-Fortuna",
+  romanName: "Fortuna",
+  romanSearchResults: 527000
+},
+{
+  greekName: "Zephyrus",
+  greekSearchResults: 132000,
+  pair: "Zephyrus-Favonius",
+  romanName: "Favonius",
+  romanSearchResults: 72100
+},
+{
+  greekName: "Zeus",
+  greekSearchResults: 13500000,
+  pair: "Zeus-Jupiter",
+  romanName: "Jupiter",
+  romanSearchResults: 10400000
+}
+];
